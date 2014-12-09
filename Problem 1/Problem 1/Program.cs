d05ea4847307a0a082f776be9179ed42283c47c0
Joe <joe.m.cullen1990@gmail.com>
2014-12-09 15:00:21 +0000

    Problem 1
    Problem 1 setup

---
+++ Problem 1/Problem 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+        //Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
