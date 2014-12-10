dd4b05b716270ed3ca3a4cf2231ac62721c21415
Joe <joe.m.cullen1990@gmail.com>
2014-12-10 00:15:48 +0000

    Start Problem 3
    Finished Problem 2 and started Problem 3

---
+++ Problem 3/Problem 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Problem_3
+{
+    class Program
+    {
+        //The prime factors of 13195 are 5, 7, 13 and 29.
+
+        //What is the largest prime factor of the number 600851475143 ?
+
+        public static bool CheckIfPrime(int value)
+        {
+            for (int i = 0; i < value; i++)
+            {
+                
+            }
+        }
+
+        public static void Iteration()
+        {
+            var possiblePrimeFactor = 2;
+
+            while (true)
+            {
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
