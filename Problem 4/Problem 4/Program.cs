ac325f6fb26ea160b10c30035e7f8f35e26302a5
Joe <joe.m.cullen1990@gmail.com>
2014-12-14 02:40:45 +0000

    Finished Problem 4 and started Problem 5

--- Problem 4/Problem 4/Program.cs
+++ Problem 4/Problem 4/Program.cs
@@ -8,8 +8,61 @@ namespace Problem_4
 {
     class Program
     {
+        //A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+        //Find the largest palindrome made from the product of two 3-digit numbers.
+
+        public static bool IsPalindromic(int value)
+        {
+            var valueAsString = value.ToString();
+
+            var charArray = valueAsString.ToCharArray();
+            Array.Reverse(charArray);
+
+            var reversedString = new string(charArray);
+
+            int reversedInt;
+
+            var parsePossible = Int32.TryParse(reversedString, out reversedInt);
+
+            if (reversedInt == value)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static List<int> Iteration()
+        {
+
+            var palindromicProducts = new List<int>();
+
+            for (int i = 100; i < 1000; i++)
+            {
+                for (int j = 100; j < 1000; j++)
+                {
+                    var product = j * i;
+
+                    if (IsPalindromic(product))
+                    {
+                        palindromicProducts.Add(product);
+                    }
+                }
+            }
+
+            return palindromicProducts;
+        }
+
         static void Main(string[] args)
         {
+            var palindromes = Iteration();
+
+            Console.WriteLine(string.Format("The largest palindromic number made from the product of two 3-digit numbers is {0}", palindromes.Max()));
+            Console.ReadLine();
+
         }
     }
 }
---
+++ Problem 5/Problem 5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_5
+{
+    class Program
+    {
+        //2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+        //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
