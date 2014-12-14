5f6bd4d1523e895ef710c318fde870ddd604622c
Joe <joe.m.cullen1990@gmail.com>
2014-12-14 03:40:22 +0000

    Completed Problems 5 and 6

--- Problem 5/Problem 5/Program.cs
+++ Problem 5/Problem 5/Program.cs
@@ -12,8 +12,57 @@ namespace Problem_5
 
         //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+        //If 'value' is divisible by 20, it is also divisible by 10, 5, 4, 2 and 1.
+        //  19
+        //If 'value' is divisible by 18, it is also divisible 9, 6 and 3.
+        //  17
+        //If 'value' is divisible by 16, it is also divisible by 8.
+        //If 'value' is divisible by 5 AND 3 (as shown above), it is also divisible by 15.
+        //If 'value' is divisible by 14, it is also divisible by 7.
+        //  13
+        //If 'value' is divisible by 3 AND 4 (as shown above), it is also divisible by 12.
+        //  11
+        //20, 19, 18, 17, 16, 14, 13, 11 are only factors we need
+
+        public static bool IsDivisibleByAllFactors(int value)
+        {
+            var factors = new List<int> { 20, 19, 18, 17, 16, 14, 13, 11 };
+
+            foreach (var factor in factors)
+            {
+                if (value % factor != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static int Iteration()
+        {
+            var value = 2520;
+
+            while (!IsDivisibleByAllFactors(value))
+            {
+                value++;
+            }
+
+            return value;
+        }
+
+
         static void Main(string[] args)
         {
+            var multiple = Iteration();
+
+            Console.WriteLine(string.Format("The smallest multiple of all integers from 1 to 20 is {0}", multiple));
+            Console.ReadLine();
+
+            // This works but is slow. You can find the lowest common multiple by breaking each factor down to it's primes (5*(2^2) = 20, 19 = 19, 2*(3^2) = 18 etc.) and picking out the greatest power of each prime
+            // (i.e. so where 2 is put to the greatest power (2^4 since this appears in prime factorisation of 16), where 3 is put to the greatest power (3^2 since this appears in prime factorisation of 18) etc.)
+            // and multiply these together (i.e. 2^4 * 3^2 * ...)
+
         }
     }
 }
---
+++ Problem 6/Problem 6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var integers = new List<int>();
+
+            for (int i = 1; i < 101; i++)
+            {
+                integers.Add(i);
+            }
+
+            var sumThenSquare = integers.Sum() * integers.Sum();
+
+            for (int j = 0; j < integers.Count; j++)
+            {
+                integers[j] = integers[j] * integers[j];
+            }
+
+            var squareThenSum = integers.Sum();
+
+            var result = 0;
+
+            if (squareThenSum > sumThenSquare)
+            {
+                result = squareThenSum - sumThenSquare;
+            }
+            else
+            {
+                result = sumThenSquare - squareThenSum;
+            }
+
+            Console.WriteLine(string.Format("The difference between the sum of the squares of the first 100 numbers and the square of the sum is {0}", result));
+            Console.ReadLine();
+        }
+    }
+}
