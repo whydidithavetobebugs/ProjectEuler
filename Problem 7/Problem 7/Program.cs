180f8360564d861ce71e8b07872d2d5e73073e0e
Joe <joe.m.cullen1990@gmail.com>
2014-12-16 01:24:21 +0000

    Start Problem 7

---
+++ Problem 7/Problem 7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_7
+{
+    class Program
+    {
+        //By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+        //What is the 10,001st prime number?
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
